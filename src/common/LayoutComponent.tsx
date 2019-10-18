c30b6ab3146bebe3f7114a41f95e363958417acb
Filipe Correia <filipe.lp.correia@gmail.com>
2019-10-18 08:15:38 +0100

    Improvs

--- src/common/LayoutComponent.tsx
+++ src/common/LayoutComponent.tsx
@@ -6,16 +6,19 @@ const IconLink = (props) => {
     return <Link to={{pathname: props.pathname}}>
         <div className='flex flex-wrap text-center'>
             <div className={classnames('fa text-4xl w-full', props.icon)}></div>
-            <div className='w-full'>{props.children}</div>
+            <div className='w-full lowercase'>{props.children}</div>
         </div>
     </Link>
 }
+
+const Divider = () => <div className='bg-blue-100 mb-2' style={{width: 1}}></div>;
 export const LayoutComponent = (props) => {
 
-    return <div className="px-2 h-screen py-4 w-screen overflow-scroll" style={{ background: 'linear-gradient(0deg, rgba(39,87,138,1) 0%, rgba(30,91,172,1) 53%, rgba(50,50,179,1) 93%)' }}>
+    return <div className="px-2 h-screen py-4 w-screen overflow-scroll text-blue-500" style={{ background: 'linear-gradient(0deg, rgba(39,87,138,1) 0%, rgba(30,91,172,1) 53%, rgba(50,50,179,1) 93%)' }}>
         {props.children}
         <div className='fixed w-full flex justify-center bg-white border-t border-white-200 bottom-0 -m-2 h-20 bg-white-100 py-2'>
             <IconLink pathname='/spend' icon='fa-wallet'> Spend</IconLink>
+            <Divider />
             <IconLink pathname='/' icon='fa-home'> Home</IconLink>
         </div>
 
